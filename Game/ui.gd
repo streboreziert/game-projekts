6d99a6eabeb9b3d25d17ad8460290f3ccfbf1857
Roberts Treize <robertstreize@gmail.com>
2025-05-07 11:42:26 +0300

    Please deleet to yoru

--- Game/ui.gd
+++ Game/ui.gd
@@ -1,13 +1,19 @@
 extends Node2D
 
-
-
 signal attributes(angle)
+
+var controlled_cars = []
+
 func _ready() -> void:
 	var default_angle = 45
-	attributes.emit(default_angle * PI / 180)
-	
+	emit_angle(default_angle)
+
 func _on_h_slider_value_changed(value: float) -> void:
 	var angle = get_node("Button/HSlider").value
-	get_node("Button/HSlider/Label").text = String.num(angle)
-	attributes.emit(PI * angle / 180)
+	get_node("Button/HSlider/Label").text = str(angle)
+	emit_angle(angle)
+
+func emit_angle(angle: float) -> void:
+	for car in controlled_cars:
+		if car.has_method("set_steering_angle"):
+			car.set_steering_angle(deg_to_rad(angle))
